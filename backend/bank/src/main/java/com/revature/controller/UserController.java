28dd6b10b1f57c675eb19210f2ffa14600d46e7b
Kenny Bui <kbui3145@gmail.com>
2019-10-10 14:26:39 -0500

    Modified usercontroller,userservice,repository

--- backend/bank/src/main/java/com/revature/controller/UserController.java
+++ backend/bank/src/main/java/com/revature/controller/UserController.java
@@ -3,6 +3,7 @@ package com.revature.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,10 @@ public class UserController {
 	public User login(@PathVariable("username")String userName, @PathVariable("password")String password) {
 		
 		return null;
+	}
+	
+	@PostMapping("/signup")
+	public void signup(User user) {
+		userService.signup(user);
 	}
 }
--- backend/bank/src/main/java/com/revature/service/UserService.java
+++ backend/bank/src/main/java/com/revature/service/UserService.java
@@ -1,8 +1,27 @@
 package com.revature.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.revature.model.User;
+import com.revature.repository.UserRepository;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	public UserRepository getUserRepository() {
+		return userRepository;
+	}
+
+	public void setUserRepository(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	public void signup(User user) {
+		userRepository.save(user);
+	}
 	
 }
